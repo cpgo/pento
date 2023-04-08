f999c88453aca7caca475d162df8aa2e1f70aa52
Cassio <cassiopgodinho@gmail.com>
2023-04-08 13:31:12 -0300

    win

--- lib/cpgo_web/live/wrong_live.ex
+++ lib/cpgo_web/live/wrong_live.ex
@@ -2,13 +2,27 @@ defmodule CpgoWeb.WrongLive do
   use CpgoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, score: 0, message: "Make a guess!!!")
+    correct_number = to_string(:rand.uniform(10))
+
+    socket = assign(socket, score: 0, message: "Make a guess!!!", correct: correct_number)
     {:ok, socket}
   end
 
-  def handle_event("guess", %{"number"=> guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again."
+  def time() do
+    DateTime.utc_now() |> to_string()
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) when guess == socket.assigns.correct do
+    message = "You win"
+    score = socket.assigns.score + 1
+
+    {:noreply, assign(socket, message: message, score: score)}
+  end
+
+  def handle_event("guess", %{"number" => _guess}, socket) do
+    message = "You lose"
     score = socket.assigns.score - 1
+
     {:noreply, assign(socket, message: message, score: score)}
   end
 
@@ -17,8 +31,8 @@ defmodule CpgoWeb.WrongLive do
     <h1>Your score: <%= @score %></h1>​
     ​ 	​
     <h2>​
-      ​ 	​ <%= @message %>​
-      ​ 	​</h2>​
+      ​ <%= @message %>​
+      ​</h2>​
     ​ 	​
     <h2>
       ​
